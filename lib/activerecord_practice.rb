fae072899f454be3152edf030da62ca9e9ae8a4e
natalie3737 <natalieh3737@gmail.com>
2025-04-28 21:51:51 -0500

    Completed Homework (4/28)

--- lib/activerecord_practice.rb
+++ lib/activerecord_practice.rb
@@ -1,6 +1,9 @@
 require 'sqlite3'
 require 'active_record'
 require 'byebug'
+require 'active_support/all'  
+Time.zone = 'UTC'
+
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'customers.sqlite3')
 # Show queries in the console.
@@ -23,10 +26,60 @@ class Customer < ApplicationRecord
   def self.any_candice
     # YOUR CODE HERE to return all customer(s) whose first name is Candice
     # probably something like:  Customer.where(....)
+    Customer.where(first: 'Candice')
   end
 
   def self.with_valid_email
     # YOUR CODE HERE to return only customers with valid email addresses (containing '@')
+    Customer.where("email LIKE ?", "%@%")
   end
   # etc. - see README.md for more details
+
+  def self.with_dot_org_email
+    where("email LIKE '%.org'")
+  end
+
+  def self.with_valid_email_and_born_before_1980
+    Customer.where("email NOT LIKE '%@%' AND email != '' AND email IS NOT NULL")
+  end
+
+  def self.with_blank_email
+    Customer.where(email: [nil,''])
+  end
+
+  def self.with_invalid_email
+    where("email NOT LIKE '%@%' AND email != ''")
+  end  
+
+  def self.born_before_1980
+    Customer.where('birthdate < ?', Date.new(1980,1,1))
+  end
+
+  def self.with_valid_email_and_born_before_1980
+    with_valid_email.where('birthdate < ?', Date.new(1980, 1, 1))
+  end  
+
+  def self.last_names_starting_with_b
+    Customer.where("last LIKE 'B%'").order(:birthdate)
+  end
+
+  def self.twenty_youngest
+    order(birthdate: :desc).limit(20)
+  end
+
+  def self.update_gussie_murray_birthdate
+    find_by(first: 'Gussie', last: 'Murray').update(birthdate: Time.parse('2004-02-08'))
+  end
+
+  def self.change_all_invalid_emails_to_blank
+    where("email NOT LIKE '%@%' AND email != '' AND email IS NOT NULL").update_all(email: '')
+  end
+
+  def self.delete_meggie_herman
+    find_by(first: "Meggie", last: "Herman").destroy
+  end
+
+  def self.delete_everyone_born_before_1978
+    where('birthdate <= ?', Date.new(1977, 12, 31)).delete_all
+  end
 end
